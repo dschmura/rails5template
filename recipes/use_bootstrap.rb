12cdfb6767f95baf6164b507b7cbcc6b96758d3f
David Chmura <dschmura@humbledaisy.com>
2018-02-04 15:03:32 -0500

    CLEANUP: formatting

--- recipes/use_bootstrap.rb
+++ recipes/use_bootstrap.rb
@@ -1,25 +1,25 @@
-  # gem 'bootstrap', '~> 4.0.0.beta3'
-  # gem 'popper_js'
-  # gem 'jquery-rails'
+# gem 'bootstrap', '~> 4.0.0.beta3'
+# gem 'popper_js'
+# gem 'jquery-rails'
 
-  insert_into_file "app/helpers/application_helper.rb", after: "ApplicationHelper\n" do
-    <<-APPHELPER
-  def bootstrap_class_for flash_type
-    { success: "alert-success", error: "alert-error", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
-  end
+insert_into_file "app/helpers/application_helper.rb", after: "ApplicationHelper\n" do
+  <<-APPHELPER
+def bootstrap_class_for flash_type
+  { success: "alert-success", error: "alert-error", alert: "alert-warning", notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+end
 
-  def flash_messages(opts = {})
-    flash.each do |msg_type, message|
-      concat(content_tag(:div, message, class: "alert \#{bootstrap_class_for(msg_type)} fade in") do
-        concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
-          concat message
-      end)
-    end
-    nil
+def flash_messages(opts = {})
+  flash.each do |msg_type, message|
+    concat(content_tag(:div, message, class: "alert \#{bootstrap_class_for(msg_type)} fade in") do
+      concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
+        concat message
+    end)
   end
+  nil
+end
 
-  APPHELPER
-  end
+APPHELPER
+end
 
 #   insert_into_file "app/assets/javascripts/application.js", after: "// about supported directives.\n//\n" do
 #     "\n//= require jquery2\n//= require popper\n//= require bootstrap\n"
