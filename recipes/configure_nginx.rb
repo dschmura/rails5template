64f182d3a67cced99ba13c81e74f0b99c03ffa9c
David Chmura <dschmura@humbledaisy.com>
2018-03-06 10:26:59 -0500

    UPDATE: Set server_name based on the app_name + _website format.

--- recipes/configure_nginx.rb
+++ recipes/configure_nginx.rb
@@ -1,13 +1,13 @@
 file 'config/nginx.conf'
 append_to_file 'config/nginx.conf' do
   <<-NGINX_CONF
-upstream puma {
-  server unix:///home/deployer/apps/#{app_name}/shared/tmp/sockets/#{app_name}-puma.sock;
+upstream #{app_name}-puma {
+  server unix:///home/deployer/apps/#{app_name}/shared/tmp/sockets/#{app_name}-puma.sock fail_timeout=0;
 }
-
 server {
-  listen 80 default_server deferred;
-  # server_name example.com;
+  listen 80;
+  listen [::]:80;
+  server_name #{app_name.gsub('_website', '')}.com;
 
   root /home/deployer/apps/#{app_name}/current/public;
   access_log /home/deployer/apps/#{app_name}/current/log/nginx.access.log;
@@ -19,19 +19,20 @@ server {
     add_header Cache-Control public;
   }
 
-  try_files deployer@production_server:~$ uri/index.html deployer@production_server:~$ uri @puma;
+  try_files $uri/index.html $uri @puma;
   location @puma {
-    proxy_set_header X-Forwarded-For deployer@production_server:~$ proxy_add_x_forwarded_for;
-    proxy_set_header Host deployer@production_server:~$ http_host;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header Host $http_host;
     proxy_redirect off;
 
-    proxy_pass http://puma_#{app_name};
+    proxy_pass http://#{app_name}-puma;
   }
 
   error_page 500 502 503 504 /500.html;
   client_max_body_size 10M;
   keepalive_timeout 10;
 }
+
 
   NGINX_CONF
 end
