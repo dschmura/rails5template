fc06417cb08bbb6b5606863deec9657ffdf3c5d4
David Chmura <dschmura@humbledaisy.com>
2018-03-06 05:59:24 -0500

    UPDATE: Webpacker refinements

--- recipes/use_webpacker.rb
+++ recipes/use_webpacker.rb
@@ -19,11 +19,10 @@ insert_into_file 'config/application.rb', after: 'config.load_defaults 5.2' do
 
 end
 
-rails_command('bundle exec rails webpacker:install')
+run 'mv app/javascript app/webpacker'
 
 # SET UP ASSETS STRUCTURE FOR WEBPACKER
-file 'app/javascript/packs/application.js'
-insert_into_file "app/javascript/packs/application.js", after: "// layout file, like app/views/layouts/application.html.erb" do
+insert_into_file "app/webpacker/packs/application.js", after: "// layout file, like app/views/layouts/application.html.erb" do
   <<-PACKS_APPLICATION_JS
 
   import $ from 'jquery'
@@ -42,8 +41,10 @@ insert_into_file "app/javascript/packs/application.js", after: "// layout file, 
   PACKS_APPLICATION_JS
 end
 
-file "app/javascript/#{app_name}/index.js"
-append_to_file "app/javascript/#{app_name}/index.js" do
+gsub_file('config/webpacker.yml',  'source_path: app/javascript', 'source_path: app/webpacker')
+
+file "app/webpacker/#{app_name}/index.js"
+append_to_file "app/webpacker/#{app_name}/index.js" do
   <<-INDEX_JS
   // Include external resources for this app_files
   import 'bootstrap'
@@ -57,8 +58,8 @@ append_to_file "app/javascript/#{app_name}/index.js" do
   INDEX_JS
 end
 
-file "app/javascript/#{app_name}/javascripts/application.js"
-append_to_file "app/javascript/#{app_name}/javascripts/application.js" do
+file "app/webpacker/#{app_name}/javascripts/application.js"
+append_to_file "app/webpacker/#{app_name}/javascripts/application.js" do
   <<-APPLICATION_JS
   $(function () {
     $('[data-toggle="tooltip"]').tooltip()
@@ -84,8 +85,8 @@ append_to_file "app/javascript/#{app_name}/javascripts/application.js" do
   APPLICATION_JS
 end
 
-file "app/javascript/#{app_name}/stylesheets/application.sass"
-append_to_file "app/javascript/#{app_name}/stylesheets/application.sass" do
+file "app/webpacker/#{app_name}/stylesheets/application.sass"
+append_to_file "app/webpacker/#{app_name}/stylesheets/application.sass" do
 <<-APPLICATION_SASS
 @import '~bootstrap/scss/functions'
 @import '~bootstrap/scss/bootstrap'
@@ -108,15 +109,15 @@ img:not([alt])
 APPLICATION_SASS
 end
 
-file "app/javascript/#{app_name}/stylesheets/_variables.sass"
+file "app/webpacker/#{app_name}/stylesheets/_variables.sass"
 
 gsub_file('app/views/layouts/application.html.haml',  "= stylesheet_link_tag    'application', media: 'all'", "= stylesheet_pack_tag 'application', media: 'all', 'data-turbolinks-track': 'reload'")
 
 gsub_file('app/views/layouts/application.html.haml',  "= javascript_include_tag 'application'", "= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload'")
 
-run "mkdir app/javascript/#{app_name}/images"
+run "mkdir app/webpacker/#{app_name}/images"
 run 'yarn add rails-ujs turbolinks jquery stimulus bourbon bootstrap babili'
 # Add popper which is a dependencie of bootstrap.
 # I am not sure why I had to run this step manually
 # to get it to work, but maybe something with a name conflict.
-run 'yarn add popper.js -D'
+run 'yarn add popper.js -D webpack-cli -D'
