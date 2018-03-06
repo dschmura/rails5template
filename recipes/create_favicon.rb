48c7b99208ddb1035d2118dac2f201f94fc557bd
David Chmura <dschmura@humbledaisy.com>
2018-03-06 10:12:58 -0500

    UPDATE: working implimentation of favicon generation and linking.

--- recipes/create_favicon.rb
+++ recipes/create_favicon.rb
@@ -1,14 +1,13 @@
-rakefile "favicon.rake" do
-  %Q{
-namespace :favicon do
-  desc "generate a favicon"
-  task generate: :environment do
-    system <<~SYSTEM
-    sketchtool export artboards \#{Rails.root}/doc/favicon.sketch --output=\#{Rails.root}/tmp
-    convert \#{Rails.root}/tmp/favicon-*.png \#{Rails.root}/public/favicon.ico
-    rm \#{Rails.root}/tmp/favicon-*
-    SYSTEM
-  end
-end
-  }
+# We will create a temporary favicon for using the faviator npm package.
+# Requires faviator be installed. To install run;
+# npm install -g faviator
+
+run ("faviator --size '230' --text #{app_name[0].upcase} --dx '-5' --dy '2' --font-size '70' --font-family 'Pacifico' --font-color '#ffffff' --background-color '#465D85' --border-width '3.5' --border-color '#97ABBF' --border-radius '50' --output app/webpacker/#{app_name}/images/favicon.png")
+
+
+append_to_file 'app/views/layouts/application.html.haml', after: '%title= page_title' do
+  <<-FAVICON
+
+    = favicon_link_tag asset_pack_path('tada/images/favicon.png')
+  FAVICON
 end
